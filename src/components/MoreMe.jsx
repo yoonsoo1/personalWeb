4d5594e606d1c13001acefd1540bb518e3866d8f
Yoonsoo Nam <yoonsoon@usc.edu>
2022-03-25 02:09:11 -0700

    changed about me page

--- src/components/MoreMe.jsx
+++ src/components/MoreMe.jsx
@@ -17,13 +17,13 @@ function MoreMe() {
                         I'm a fourth year Computer Science and Economics major at USC. You can find me hanging out in Los Angeles, CA, unless I'm in Busan, Korea for a short break!
                     </p>
                     <p>
-                        I am a risk taker and an adventurer. I was originally a Economics and Accounting major at UCSB. After getting into the major, I went to the Korean military. There, I found my passion for building software to increase efficency. I came back and transferred to USC so that I could at least attempt to take CS courses.
+                        I am a risk taker and an adventurer. I was originally a Economics and Accounting major at UCSB. After getting into the major, I went to the Korean military. There, I found opportunities to build software applications to increase efficiency of workflow. I came back and transferred to USC so that I could pursue my curiosity that soon became my passion.
                     </p>
                     <p>
-                        My junior year was spent fighting for my chances to get into the CS major at USC. It was a big risk. If I did not make it, I would waste a whole year of college, which I am already behind in due to my service. When I decide to take on an adventure, I commit 100% and make it happen. 
+                        My junior year was spent fighting for my chances to get into the CS major at USC. It was a big risk. If I did not make it, I would have wasted a whole year of college, which I was already behind in due to my service. However, when I decide to take on an adventure, I commit 100% and make it happen. 
                     </p>
                     <p>
-                        My serious passion for development lies in Full Stack development. I love that I can build something to benefit the people around me.
+                        My serious passion for development lies in Full Stack development. You can check out some cool things I built for my class: <a href="http://303.itpwebdev.com/~yoonsoon/student_page.html">Full Stack Development Coursework</a>. I love that I have the potential to build apps that can directly benefit the people around me.
                     </p>
                     <div className="resume-btn">
                         <a href= {EngResume} target = "_blank"><AniButton text='My Resume [Eng]'></AniButton></a>
